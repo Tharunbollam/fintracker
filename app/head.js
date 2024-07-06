2eafb2b8d7f71991455a04c18aceb81cb51e163d
Tharun <bt21eeb0a10@student.nitw.ac.in>
2024-07-06 11:39:11 +0530

    removed head

--- app/head.js
+++
@@ -1,9 +0,0 @@
-export default function Head(){
-    return (
-        <>
-        <title>Finance Tracker</title>
-        <meta content="width=device-width, initial-scale=1"></meta>
-        <link rel="icon" href="/favicon.ico"></link>
-        </>
-    );
-}
